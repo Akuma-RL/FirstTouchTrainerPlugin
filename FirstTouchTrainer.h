e8303b07557c34693cdbd4d772b12fc814d5a482
Akuma <35553250+MythosShaitan@users.noreply.github.com>
2024-11-02 00:28:59 -0700

    added a session timer to the plugin

--- FirstTouchTrainer.h
+++ FirstTouchTrainer.h
@@ -6,6 +6,7 @@
 #include "bakkesmod/plugin/PluginSettingsWindow.h"
 
 #include "version.h"
+
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
 
@@ -16,25 +17,46 @@ protected:
 	//osd enabled/disabled
 	std::shared_ptr<bool>bEnabled;
 
+	//global screen size
+	std::shared_ptr<float>bScreenSizeX;
+	std::shared_ptr<float>bScreenSizeY;
+
 	//text settings
-	std::shared_ptr<int>XPos;
-	std::shared_ptr<int>YPos;
-	std::shared_ptr<int>TextSize;
-	std::shared_ptr<bool>bDropShadow;
+	std::shared_ptr<int>tXPos;
+	std::shared_ptr<int>tYPos;
+	std::shared_ptr<int>tTextSize;
+	std::shared_ptr<bool>tDropShadow;
+
+	std::shared_ptr<float>tDefaultXPos;
+	std::shared_ptr<float>tDefaultYPos;
 
 	//text color settings
-	std::shared_ptr<LinearColor>goodColor;
-	std::shared_ptr<LinearColor>alrightColor;
-	std::shared_ptr<LinearColor>badColor;
+	std::shared_ptr<LinearColor>cGoodColor;
+	std::shared_ptr<LinearColor>cAlrightColor;
+	std::shared_ptr<LinearColor>cBadColor;
+
+	//session enabled
+	std::shared_ptr<bool>sSessionTimerEnabled;
+	std::shared_ptr<int>sSessionTimerX;
+	std::shared_ptr<int>sSessionTimerY;
+	std::shared_ptr<LinearColor>sSessionTimerColor;
+
+
+	std::shared_ptr<float>sDefaultXPos;
+	std::shared_ptr<float>sDefaultYPos;
+
 
 public:
 	void onLoad() override;
 	void onUnload() override;
 
 	virtual void RenderSettings() override;
+
 	void Render(CanvasWrapper canvas);
 	std::tuple<float> firstTouchTrainer();
 
 	int checkConditions();
 	std::string toStringPrecision(float InValue, int Precision);
+
+	int timerCheckConditions();
 };
--- RenderSettings.cpp
+++ RenderSettings.cpp
@@ -7,140 +7,236 @@ void FirstTouchTrainer::RenderSettings()
 	ImGui::TextUnformatted("Plugin to see the speed differential between your car, and the ball!");
 
 	//enable plugin checkbox
-	CVarWrapper enablePlugin = cvarManager->getCvar("FTT_Enable");
-	if (!enablePlugin) { return; }
-	bool pluginEnabled = enablePlugin.getBoolValue();
+	bool pluginEnabled = *bEnabled;
 	if (ImGui::Checkbox("Enable/Disable OSD", &pluginEnabled)) {
-		enablePlugin.setValue(pluginEnabled);
+		*bEnabled = pluginEnabled;
 	}
 	if (ImGui::IsItemHovered()) {
 		ImGui::SetTooltip("Enable/Disable On Screen Display");
 	}
+	if (pluginEnabled)
+	{
+		//checkbox for drop shadows
+		bool shadowEnabled = *tDropShadow;
+		if (ImGui::Checkbox("Enable/Disable Drop Shadow", &shadowEnabled)) {
+			*tDropShadow = shadowEnabled;
+		}
+		if (ImGui::IsItemHovered()) {
+			ImGui::SetTooltip("Enable/Disable Text Drop Shadows");
+		}
 
-	//text X Position Slider
-	ImGui::TextUnformatted("Text Position Sliders");
-	CVarWrapper xPosCvar = cvarManager->getCvar("FTT_X_Position");
-	if (!xPosCvar) { return; }
-	float posX = xPosCvar.getFloatValue();
-	if (ImGui::SliderFloat("Horizontal Position", &posX, 0.0, 3840)) {
-		xPosCvar.setValue(posX);
-	}
-	if (ImGui::IsItemHovered()) {
-		std::string hoverXText = "X position is " + std::to_string(posX);
-		ImGui::SetTooltip(hoverXText.c_str());
-	}
+		if (ImGui::TreeNode("Speed Display Settings"))
+		{
+			static bool closable_group = true;
 
-	//text Y Position Slider
-	CVarWrapper yPosCvar = cvarManager->getCvar("FTT_Y_Position");
-	if (!yPosCvar) { return; }
-	float posY = yPosCvar.getFloatValue();
-	if (ImGui::SliderFloat("Vertical Position", &posY, 0.0, 2160)) {
-		yPosCvar.setValue(posY);
-	}
-	if (ImGui::IsItemHovered()) {
-		std::string hoverYText = "Y position is " + std::to_string(posY);
-		ImGui::SetTooltip(hoverYText.c_str());
-	}
+			//text size slider
+			ImGui::TextUnformatted("Text Size Slider");
 
-	//text size slider
-	ImGui::TextUnformatted("Text Size Slider");
-	CVarWrapper textSizeCvar = cvarManager->getCvar("FTT_Text_Size");
-	if (!textSizeCvar) { return; }
-	int textSize = textSizeCvar.getIntValue();
-	if (ImGui::SliderInt("Text Size", &textSize, 1, 10)) {
-		textSizeCvar.setValue(textSize);
-	}
-	if (ImGui::IsItemHovered()) {
-		std::string hoverTextSize = "Text Size is " + std::to_string(textSize);
-		ImGui::SetTooltip(hoverTextSize.c_str());
-	}
+			int textSize = *tTextSize;
+			if (ImGui::SliderInt("Text Size", &textSize, 1, 10)) {
+				*tTextSize = textSize;
+			}
+			if (ImGui::IsItemHovered()) {
+				std::string hoverTextSize = "Text Size is " + std::to_string(textSize);
+				ImGui::SetTooltip(hoverTextSize.c_str());
+			}
 
-	//checkbox for drop shadows
-	CVarWrapper enableShadow = cvarManager->getCvar("FTT_Shadow");
-	if (!enableShadow) { return; }
-	bool shadowEnabled = enableShadow.getBoolValue();
-	if (ImGui::Checkbox("Enable/Disable Drop Shadow", &shadowEnabled)) {
-		enableShadow.setValue(shadowEnabled);
-	}
-	if (ImGui::IsItemHovered()) {
-		ImGui::SetTooltip("Enable/Disable Text Drop Shadows");
-	}
+			//Speed text X Position Slider
+			ImGui::TextUnformatted("Speed Text Position");
+			float posX = *tXPos;
+			if (ImGui::SliderFloat("Horizontal Position", &posX, 0.0f, *bScreenSizeX)) {
+				*tXPos = posX;
+			}
+			if (ImGui::IsItemHovered()) {
+				std::string hoverXText = "Speed Text X position is " + std::to_string(posX);
+				ImGui::SetTooltip(hoverXText.c_str());
+			}
 
-	//reset sliders to default
-	if (ImGui::Button("Reset Defaults")) {
-		xPosCvar.setValue(880.0f);
-		yPosCvar.setValue(1030.0f);
-		textSizeCvar.setValue(3);
-		enableShadow.setValue(true);
+			//Speed text Y Position Slider
+			float posY = *tYPos;
+			if (ImGui::SliderFloat("Vertical Position", &posY, 0.0f, *bScreenSizeY)) {
+				*tYPos = posY;
+			}
+			if (ImGui::IsItemHovered()) {
+				std::string hoverYText = "Speed Text Y position is " + std::to_string(posY);
+				ImGui::SetTooltip(hoverYText.c_str());
+			}
+			int textDefaultSize = 3;
+			bool textDefaultDropShadow = true;
+
+			//reset sliders to default
+			if (ImGui::Button("Reset Defaults")) {
+				*tXPos = *tDefaultXPos;
+				*tYPos = *tDefaultYPos;
+				*tTextSize = textDefaultSize;
+				*tDropShadow = textDefaultDropShadow;
+			}
+
+			//Getting color data from user for the "good color"
+			LinearColor textGoodColor = *cGoodColor / 255;
+			if (ImGui::ColorEdit4("Good Range Color", &textGoodColor.R)) {
+				*cGoodColor = textGoodColor * 255;
+			}
+			if (ImGui::IsItemHovered()) {
+				ImGui::SetTooltip("Set color of the good touch speed range");
+			}
+
+			//Getting color data from user for the "alright color"
+			LinearColor textAlrightColor = *cAlrightColor / 255;
+			if (ImGui::ColorEdit4("Alright Range Color", &textAlrightColor.R)) {
+				*cAlrightColor = textAlrightColor * 255;
+			}
+			if (ImGui::IsItemHovered()) {
+				ImGui::SetTooltip("Set color of the alright touch speed range");
+			}
+
+			//Getting color data from user for the "bad color"
+			LinearColor textBadColor = *cBadColor / 255;
+			if (ImGui::ColorEdit4("Bad Range Color", &textBadColor.R)) {
+				*cBadColor = textBadColor * 255;
+			}
+			if (ImGui::IsItemHovered()) {
+				ImGui::SetTooltip("Set color of the bad touch speed range");
+			}
+
+			LinearColor defaultGoodColor(0, 1, 0, 1);
+			LinearColor defaultAlrightColor(1, 1, 0, 1);
+			LinearColor defaultBadColor(1, 0, 0, 1);
+
+			if (ImGui::Button("Default Colors")) {
+				*cGoodColor = defaultGoodColor * 255;
+				*cAlrightColor = defaultAlrightColor * 255;
+				*cBadColor = defaultBadColor * 255;
+			}
+			if (ImGui::IsItemHovered()) {
+				ImGui::SetTooltip("Reset the colors to the default value assigned to them");
+			}
+			ImGui::TreePop();
+		}
 	}
 
 	ImGui::TextUnformatted("\n");
 
-	//Getting color data from user for the "good color"
-	CVarWrapper goodRange = cvarManager->getCvar("FTT_Good_Range");
-	if (!goodRange) { return; }
-	LinearColor textGoodColor = goodRange.getColorValue() / 255;
-	if (ImGui::ColorEdit4("Good Range Color", &textGoodColor.R)) {
-		goodRange.setValue(textGoodColor * 255);
+	//enable session timer
+	bool sessionTimerEnabled = *sSessionTimerEnabled;
+	if (ImGui::Checkbox("Enable/Disable Session Timer", &sessionTimerEnabled)) {
+		*sSessionTimerEnabled = sessionTimerEnabled;
 	}
-	*goodColor = textGoodColor * 255;
 	if (ImGui::IsItemHovered()) {
-		ImGui::SetTooltip("Set color of the good touch speed range");
+		ImGui::SetTooltip("Enable/Disable Session Timer");
 	}
+	if (sessionTimerEnabled)
+	{
+		if (ImGui::TreeNode("Session Timer Display Settings"))
+		{
+			static bool closable_group = true;
 
-	//Getting color data from user for the "alright color"
-	CVarWrapper alrightRange = cvarManager->getCvar("FTT_Alright_Range");
-	if (!alrightRange) { return; }
-	LinearColor textAlrightColor = alrightRange.getColorValue() / 255;
-	if (ImGui::ColorEdit4("Alright Range Color", &textAlrightColor.R)) {
-		alrightRange.setValue(textAlrightColor * 255);
-	}
-	*alrightColor = textAlrightColor * 255;
-	if (ImGui::IsItemHovered()) {
-		ImGui::SetTooltip("Set color of the alright touch speed range");
-	}
 
-	//Getting color data from user for the "bad color"
-	CVarWrapper badRange = cvarManager->getCvar("FTT_Bad_Range");
-	if (!badRange) { return; }
-	LinearColor textBadColor = badRange.getColorValue() / 255;
-	if (ImGui::ColorEdit4("Bad Range Color", &textBadColor.R)) {
-		badRange.setValue(textBadColor * 255);
-	} 
-	*badColor = textBadColor * 255;
-	if (ImGui::IsItemHovered()) {
-		ImGui::SetTooltip("Set color of the bad touch speed range");
-	}
 
+			float sessionXPos = *sSessionTimerX;
+			if (ImGui::SliderFloat("Horizontal Position##2", &sessionXPos, 0.0f, *bScreenSizeX)) {
+				*sSessionTimerX = sessionXPos;
+			}
+			if (ImGui::IsItemHovered()) {
+				std::string timerXText = "Session timer X position is " + std::to_string(sessionXPos);
+				ImGui::SetTooltip(timerXText.c_str());
+			}
+
+			float sessionYPos = *sSessionTimerY;
+			if (ImGui::SliderFloat("Vertical Position##2", &sessionYPos, 0.0f, *bScreenSizeY)) {
+				*sSessionTimerY = sessionYPos;
+			}
+			if (ImGui::IsItemHovered()) {
+				std::string timerYText = "Session timer Y position is " + std::to_string(sessionYPos);
+				ImGui::SetTooltip(timerYText.c_str());
+			}
+			//reset sliders to default
+			if (ImGui::Button("Reset Defaults##2")) {
+				*sSessionTimerX = *sDefaultXPos;
+				*sSessionTimerY = *sDefaultYPos;
+			}
+
+			//Getting color data from user for the session timer
+			LinearColor textSessionTimerColor = *sSessionTimerColor / 255;
+			if (ImGui::ColorEdit4("Sesion Timer Color", &textSessionTimerColor.R)) {
+				*sSessionTimerColor = textSessionTimerColor * 255;
+			}
+			if (ImGui::IsItemHovered()) {
+				ImGui::SetTooltip("Set color of the session timer");
+			}
+			LinearColor defaultSessionTimercolor(1, 1, 1, 1);
+			if (ImGui::Button("Default Colors##2")) {
+				*sSessionTimerColor = defaultSessionTimercolor * 255;
+			}
+			if (ImGui::IsItemHovered()) {
+				ImGui::SetTooltip("Reset the colors to the default value assigned to them");
+			}
+
+			ImGui::TreePop();
+		}
+	}
 	return;
 }
 
+
 void FirstTouchTrainer::Render(CanvasWrapper canvas)
 {
-	if (checkConditions() == 1) { return; }
+	if (checkConditions() == 1)
+	{
 
-	auto screenSize = canvas.GetSize();
-	canvas.SetPosition(Vector2{ *XPos, *YPos });
+		canvas.SetPosition(Vector2{ *tXPos, *tYPos });
 
-	float drawVelocity = std::get<0>(firstTouchTrainer());
+		float drawVelocity = std::get<0>(firstTouchTrainer());
 
-	//set colors at certain velocity
-	if (-150.0f <= drawVelocity && drawVelocity <= 150.0f) {
-		canvas.SetColor(*goodColor);
-	}
-	else if (-300.0f <= drawVelocity && drawVelocity <= 300.0f) {
-		canvas.SetColor(*alrightColor);
-	}
-	else {
-		canvas.SetColor(*badColor);
+		//set colors at certain velocity
+		if (-150.0f <= drawVelocity && drawVelocity <= 150.0f) {
+			canvas.SetColor(*cGoodColor);
+		}
+		else if (-300.0f <= drawVelocity && drawVelocity <= 300.0f) {
+			canvas.SetColor(*cAlrightColor);
+		}
+		else {
+			canvas.SetColor(*cBadColor);
+		}
+
+		if (gameWrapper->GetbMetric()) {
+			canvas.DrawString(toStringPrecision(drawVelocity * .036f, 2) + " KPH", *tTextSize, *tTextSize, *tDropShadow);
+		}
+		else {
+			canvas.DrawString(toStringPrecision(drawVelocity / 44.704f, 2) + " MPH", *tTextSize, *tTextSize, *tDropShadow);
+		}
 	}
 
-	if (gameWrapper->GetbMetric()) {
-		canvas.DrawString(toStringPrecision(drawVelocity * .036f, 2) + " KPH", *TextSize, *TextSize, *bDropShadow);
-	}
-	else {
-		canvas.DrawString(toStringPrecision(drawVelocity / 44.704f, 2) + " MPH", *TextSize, *TextSize, *bDropShadow);
-	}
+	//check if session timer is enabled
+	if (timerCheckConditions() == 1)
+	{
 
+		//get seconds elapsed
+		auto server = gameWrapper->GetGameEventAsServer();
+		int startSessionTimer = server.GetSecondsElapsed();
+
+		//set session timer color
+		canvas.SetColor(*sSessionTimerColor);
+
+		//convert timer to int
+		int t = startSessionTimer;
+
+		//3600 seconds in an hour
+		t %= 3600;
+
+		//divide by 60 to get 60 minutes in the hour
+		int timerMinutes = t / 60;
+
+		//eveery 60 seconds convert to 1 minute
+		t %= 60;
+		int timerSeconds = t;
+
+		char timerBuffer[50];
+		sprintf_s(timerBuffer, "%dm%ds", timerMinutes, timerSeconds);
+		std::string timerText = timerBuffer;
+
+		canvas.SetPosition(Vector2{ *sSessionTimerX, *sSessionTimerY });
+		canvas.DrawString(timerText, *tTextSize, *tTextSize, *tDropShadow);
+	}
+	return;
 }
-
